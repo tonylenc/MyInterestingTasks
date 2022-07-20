505526ab817f8425d89ec7f4583d11081bbae616
tonylenc <107024092+tonylenc@users.noreply.github.com>
2022-07-20 11:32:16 +0500

    Add files via upload

---
+++ Sieve_of_Eratosthenes.py
@@ -0,0 +1,18 @@
+def sieve(n):
+    prime_numbers = []
+    for i in range(n + 1):
+        prime_numbers.append(i)
+
+    prime_numbers[1] = 0
+    i = 2
+    while i <= n:
+        if prime_numbers[i] != 0:
+            j = i + i
+            while j <= n:
+                prime_numbers[j] = 0
+                j = j + i
+        i += 1
+
+    prime_numbers = set(prime_numbers)
+    prime_numbers.remove(0)
+    return sorted(prime_numbers)
