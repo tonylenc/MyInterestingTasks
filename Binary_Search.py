4d63de6b346c01c029c3c236c9cec136cd2e240b
tonylenc <107024092+tonylenc@users.noreply.github.com>
2022-09-14 21:36:56 +0500

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,17 +1,47 @@
-def binary_search(array, x):
+def binary_search(seq, x):
     left = 0
-    right = len(array) - 1
+    right = len(seq) - 1
 
     while left <= right:
         mid = (left + right) // 2
 
-        if array[mid] == x:
+        if seq[mid] == x:
             return mid
 
-        elif array[mid] < x:
+        elif seq[mid] < x:
             left = mid + 1
 
         else:
             right = mid - 1
 
     return -1
+
+
+def left_binary_search(seq, x):
+    left = -1
+    right = len(seq) - 1
+
+    while right - left > 1:
+        mid = (left + right) // 2
+
+        if seq[mid] >= x:
+            right = mid
+        else:
+            left = mid
+
+    return right
+
+
+def right_binary_search(seq, x):
+    left = 0
+    right = len(seq)
+
+    while right - left > 1:
+        mid = (left + right) // 2
+
+        if seq[mid] > x:
+            right = mid
+        else:
+            left = mid
+
+    return left
